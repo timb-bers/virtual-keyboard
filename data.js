47cfbbaaf0f32e6438be0a2a620d2d2d2225c454
Aliaksandr Prudnikau <timberprud@gmail.com>
2023-05-01 20:23:58 +0300

    feat: add key value

---
+++ data.js
@@ -0,0 +1,727 @@
+const Row1 = [
+  {
+    keyCode: 192,
+    code: 'backquote',
+    controlElement: false,
+    data: {
+      en: '`',
+      ru: 'Ё',
+    },
+    shift: {
+      en: '~',
+      ru: 'Ё',
+    },
+  },
+  {
+    keyCode: 49,
+    code: 'digit1',
+    controlElement: false,
+    data: {
+      en: '1',
+      ru: '1',
+    },
+    shift: {
+      en: '!',
+      ru: '!',
+    },
+  },
+  {
+    keyCode: 50,
+    code: 'digit2',
+    controlElement: false,
+    data: {
+      en: '2',
+      ru: '2',
+    },
+    shift: {
+      en: '@',
+      ru: '"',
+    },
+  },
+  {
+    keyCode: 51,
+    code: 'digit3',
+    controlElement: false,
+    data: {
+      en: '3',
+      ru: '3',
+    },
+    shift: {
+      en: '#',
+      ru: '№',
+    },
+  },
+  {
+    keyCode: 52,
+    code: 'digit4',
+    controlElement: false,
+    data: {
+      en: '4',
+      ru: '4',
+    },
+    shift: {
+      en: '$',
+      ru: ';',
+    },
+  },
+  {
+    keyCode: 53,
+    code: 'digit5',
+    controlElement: false,
+    data: {
+      en: '5',
+      ru: '5',
+    },
+    shift: {
+      en: '%',
+      ru: '%',
+    },
+  },
+  {
+    keyCode: 54,
+    code: 'digit6',
+    controlElement: false,
+    data: {
+      en: '6',
+      ru: '6',
+    },
+    shift: {
+      en: '^',
+      ru: ':',
+    },
+  },
+  {
+    keyCode: 55,
+    code: 'digit7',
+    controlElement: false,
+    data: {
+      en: '7',
+      ru: '7',
+    },
+    shift: {
+      en: '&',
+      ru: '?',
+    },
+  },
+  {
+    keyCode: 56,
+    code: 'digit8',
+    controlElement: false,
+    data: {
+      en: '8',
+      ru: '8',
+    },
+    shift: {
+      en: '*',
+      ru: '*',
+    },
+  },
+  {
+    keyCode: 57,
+    code: 'digit9',
+    controlElement: false,
+    data: {
+      en: '9',
+      ru: '9',
+    },
+    shift: {
+      en: '(',
+      ru: '(',
+    },
+  },
+  {
+    keyCode: 48,
+    code: 'digit0',
+    controlElement: false,
+    data: {
+      en: '0',
+      ru: '0',
+    },
+    shift: {
+      en: ')',
+      ru: ')',
+    },
+  },
+  {
+    keyCode: 189,
+    code: 'minus',
+    controlElement: false,
+    data: {
+      en: '-',
+      ru: '-',
+    },
+    shift: {
+      en: '_',
+      ru: '_',
+    },
+  },
+  {
+    keyCode: 187,
+    code: 'equal',
+    controlElement: false,
+    data: {
+      en: '=',
+      ru: '=',
+    },
+    shift: {
+      en: '+',
+      ru: '+',
+    },
+  },
+  {
+    keyCode: 8,
+    code: 'backspace',
+    controlElement: true,
+    data: 'BACKSPACE',
+  },
+];
+const Row2 = [
+  {
+    keyCode: 9,
+    code: 'tab',
+    controlElement: true,
+    data: 'TAB',
+  },
+  {
+    keyCode: 81,
+    code: 'KeyQ',
+    controlElement: false,
+    data: {
+      en: 'Q',
+      ru: 'Й',
+    },
+    shift: {
+      en: 'Q',
+      ru: 'Й',
+    },
+  },
+  {
+    keyCode: 87,
+    code: 'keyW',
+    controlElement: false,
+    data: {
+      en: 'W',
+      ru: 'Ц',
+    },
+    shift: {
+      en: 'W',
+      ru: 'Ц',
+    },
+  },
+  {
+    keyCode: 69,
+    code: 'keyE',
+    controlElement: false,
+    data: {
+      en: 'E',
+      ru: 'У',
+    },
+    shift: {
+      en: 'E',
+      ru: 'У',
+    },
+  },
+  {
+    keyCode: 82,
+    code: 'keyR',
+    controlElement: false,
+    data: {
+      en: 'R',
+      ru: 'К',
+    },
+    shift: {
+      en: 'R',
+      ru: 'К',
+    },
+  },
+  {
+    keyCode: 84,
+    code: 'keyT',
+    controlElement: false,
+    data: {
+      en: 'T',
+      ru: 'Е',
+    },
+    shift: {
+      en: 'T',
+      ru: 'Е',
+    },
+  },
+  {
+    keyCode: 89,
+    code: 'keyY',
+    controlElement: false,
+    data: {
+      en: 'Y',
+      ru: 'Н',
+    },
+    shift: {
+      en: 'Y',
+      ru: 'Н',
+    },
+  },
+  {
+    keyCode: 85,
+    code: 'keyU',
+    controlElement: false,
+    data: {
+      en: 'U',
+      ru: 'Г',
+    },
+    shift: {
+      en: 'U',
+      ru: 'Г',
+    },
+  },
+  {
+    keyCode: 73,
+    code: 'keyI',
+    controlElement: false,
+    data: {
+      en: 'I',
+      ru: 'Ш',
+    },
+    shift: {
+      en: 'I',
+      ru: 'Ш',
+    },
+  },
+  {
+    keyCode: 79,
+    code: 'keyO',
+    controlElement: false,
+    data: {
+      en: 'O',
+      ru: 'Щ',
+    },
+    shift: {
+      en: 'O',
+      ru: 'Щ',
+    },
+  },
+  {
+    keyCode: 80,
+    code: 'keyP',
+    controlElement: false,
+    data: {
+      en: 'P',
+      ru: 'З',
+    },
+    shift: {
+      en: 'P',
+      ru: 'З',
+    },
+  },
+  {
+    keyCode: 219,
+    code: 'bracketLeft',
+    controlElement: false,
+    data: {
+      en: '[',
+      ru: 'Х',
+    },
+    shift: {
+      en: '{',
+      ru: 'Х',
+    },
+  },
+  {
+    keyCode: 221,
+    code: 'bracketRight',
+    controlElement: false,
+    data: {
+      en: ']',
+      ru: 'Ъ',
+    },
+    shift: {
+      en: '}',
+      ru: 'Ъ',
+    },
+  },
+  {
+    keyCode: 220,
+    code: 'backslash',
+    controlElement: false,
+    data: {
+      en: '\\',
+      ru: '\\',
+    },
+    shift: {
+      en: '|',
+      ru: '/',
+    },
+  },
+  {
+    keyCode: 46,
+    code: 'Delete',
+    controlElement: true,
+    data: 'Del',
+  },
+];
+const Row3 = [
+  {
+    keyCode: 20,
+    code: 'capsLock',
+    controlElement: true,
+    data: 'CAPS LOCK',
+  },
+  {
+    keyCode: 65,
+    code: 'keyA',
+    controlElement: false,
+    data: {
+      en: 'A',
+      ru: 'Ф',
+    },
+    shift: {
+      en: 'A',
+      ru: 'Ф',
+    },
+  },
+  {
+    keyCode: 83,
+    code: 'keyS',
+    controlElement: false,
+    data: {
+      en: 'S',
+      ru: 'Ы',
+    },
+    shift: {
+      en: 'S',
+      ru: 'Ы',
+    },
+  },
+  {
+    keyCode: 68,
+    code: 'keyD',
+    controlElement: false,
+    data: {
+      en: 'D',
+      ru: 'В',
+    },
+    shift: {
+      en: 'D',
+      ru: 'В',
+    },
+  },
+  {
+    keyCode: 70,
+    code: 'keyF',
+    controlElement: false,
+    data: {
+      en: 'F',
+      ru: 'А',
+    },
+    shift: {
+      en: 'F',
+      ru: 'А',
+    },
+  },
+  {
+    keyCode: 71,
+    code: 'keyG',
+    controlElement: false,
+    data: {
+      en: 'G',
+      ru: 'П',
+    },
+    shift: {
+      en: 'G',
+      ru: 'П',
+    },
+  },
+  {
+    keyCode: 72,
+    code: 'keyH',
+    controlElement: false,
+    data: {
+      en: 'H',
+      ru: 'Р',
+    },
+    shift: {
+      en: 'H',
+      ru: 'Р',
+    },
+  },
+  {
+    keyCode: 74,
+    code: 'keyJ',
+    controlElement: false,
+    data: {
+      en: 'J',
+      ru: 'О',
+    },
+    shift: {
+      en: 'J',
+      ru: 'О',
+    },
+  },
+  {
+    keyCode: 75,
+    code: 'keyK',
+    controlElement: false,
+    data: {
+      en: 'K',
+      ru: 'Л',
+    },
+    shift: {
+      en: 'K',
+      ru: 'Л',
+    },
+  },
+  {
+    keyCode: 76,
+    code: 'keyL',
+    controlElement: false,
+    data: {
+      en: 'L',
+      ru: 'Д',
+    },
+    shift: {
+      en: 'L',
+      ru: 'Д',
+    },
+  },
+  {
+    keyCode: 186,
+    code: 'semicolon',
+    controlElement: false,
+    data: {
+      en: ';',
+      ru: 'Ж',
+    },
+    shift: {
+      en: ':',
+      ru: 'Ж',
+    },
+  },
+  {
+    keyCode: 222,
+    code: 'quote',
+    controlElement: false,
+    data: {
+      en: '\'',
+      ru: 'Э',
+    },
+    shift: {
+      en: '"',
+      ru: 'Э',
+    },
+  },
+  {
+    keyCode: 13,
+    code: 'enter',
+    controlElement: true,
+    data: 'ENTER',
+  },
+];
+const Row4 = [
+  {
+    keyCode: 16,
+    code: 'shiftLeft',
+    controlElement: true,
+    data: 'SHIFT',
+  },
+  {
+    keyCode: 90,
+    code: 'keyZ',
+    controlElement: false,
+    data: {
+      en: 'Z',
+      ru: 'Я',
+    },
+    shift: {
+      en: 'Z',
+      ru: 'Я',
+    },
+  },
+  {
+    keyCode: 88,
+    code: 'keyX',
+    controlElement: false,
+    data: {
+      en: 'X',
+      ru: 'Ч',
+    },
+    shift: {
+      en: 'X',
+      ru: 'Ч',
+    },
+  },
+  {
+    keyCode: 67,
+    code: 'keyC',
+    controlElement: false,
+    data: {
+      en: 'C',
+      ru: 'С',
+    },
+    shift: {
+      en: 'C',
+      ru: 'С',
+    },
+  },
+  {
+    keyCode: 86,
+    code: 'keyV',
+    controlElement: false,
+    data: {
+      en: 'V',
+      ru: 'М',
+    },
+    shift: {
+      en: 'V',
+      ru: 'М',
+    },
+  },
+  {
+    keyCode: 66,
+    code: 'keyB',
+    controlElement: false,
+    data: {
+      en: 'B',
+      ru: 'И',
+    },
+    shift: {
+      en: 'B',
+      ru: 'И',
+    },
+  },
+  {
+    keyCode: 78,
+    code: 'keyN',
+    controlElement: false,
+    data: {
+      en: 'N',
+      ru: 'Т',
+    },
+    shift: {
+      en: 'N',
+      ru: 'Т',
+    },
+  },
+  {
+    keyCode: 77,
+    code: 'keyM',
+    controlElement: false,
+    data: {
+      en: 'M',
+      ru: 'Ь',
+    },
+    shift: {
+      en: 'M',
+      ru: 'Ь',
+    },
+  },
+  {
+    keyCode: 188,
+    code: 'comma',
+    controlElement: false,
+    data: {
+      en: ',',
+      ru: 'Б',
+    },
+    shift: {
+      en: '<',
+      ru: 'Б',
+    },
+  },
+  {
+    keyCode: 190,
+    code: 'period',
+    controlElement: false,
+    data: {
+      en: '.',
+      ru: 'Ю',
+    },
+    shift: {
+      en: '>',
+      ru: 'Ю',
+    },
+  },
+  {
+    keyCode: 191,
+    code: 'slash',
+    controlElement: false,
+    data: {
+      en: '/',
+      ru: '.',
+    },
+    shift: {
+      en: '?',
+      ru: ',',
+    },
+  },
+  {
+    keyCode: 38,
+    code: 'arrowUp',
+    controlElement: true,
+    data: '▲',
+  },
+  {
+    keyCode: 16,
+    code: 'shiftRight',
+    controlElement: true,
+    data: 'SHIFT',
+  },
+];
+const Row5 = [
+  {
+    keyCode: 17,
+    code: 'controlLeft',
+    controlElement: true,
+    data: 'CTRL',
+  },
+  {
+    keyCode: 91,
+    code: 'metaLeft',
+    controlElement: true,
+    data: 'WIN',
+  },
+  {
+    keyCode: 18,
+    code: 'altLeft',
+    controlElement: true,
+    data: 'ALT',
+  },
+  {
+    keyCode: 32,
+    code: 'space',
+    controlElement: true,
+    data: '',
+  },
+  {
+    keyCode: 18,
+    code: 'altRight',
+    controlElement: true,
+    data: 'ALT',
+  },
+  {
+    keyCode: 37,
+    code: 'arrowLeft',
+    controlElement: true,
+    data: '◄',
+  },
+  {
+    keyCode: 40,
+    code: 'arrowDown',
+    controlElement: true,
+    data: '▼',
+  },
+  {
+    keyCode: 39,
+    code: 'arrowRight',
+    controlElement: true,
+    data: '►',
+  },
+  {
+    keyCode: 17,
+    code: 'controlRight',
+    controlElement: true,
+    data: 'CTRL',
+  },
+];
+
+const mass = [Row1, Row2, Row3, Row4, Row5];
+
+export default mass;
